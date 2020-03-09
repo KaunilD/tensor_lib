4850d23113a803c37bb99f076d2a7331e42b7f10
kaunild <dhruv.kaunil@gmail.com>
2020-03-09 14:19:44 -0600

    edited main.cpp

--- examples/main.cpp
+++ examples/main.cpp
@@ -2,11 +2,12 @@
 #include "host_tensor.hpp"
 #include "device_tensor.hpp"
 
-#define LOG(x) {std::cout << x << std::endl;}
+#define LOG(x) std::cout << x << std::endl
 
 using namespace std;
 
 int main()
 {
+	LOG("Hello Cmake.");
 	return 0;
 }
